623dbe6d0a2868cc7c77af4a7af6d6ca42742e60
Nana <nanayaw.net@live.com>
2020-07-10 22:02:34 -0400

    bug fix1

--- ncounter/test_lambda_function.py
+++ ncounter/test_lambda_function.py
@@ -6,4 +6,4 @@ class TestLambda(unittest.TestCase):
     def test_lambda_handler(self):
         # Testing lambda function
         print ("Running unit test...")
-        self.assertEqual(lambda_handler("status","test")["code"],200)
+        self.assertEqual(lambda_handler("status","test")["statusCode"],200)
